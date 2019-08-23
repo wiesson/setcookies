0b7aa4c79dc6b6e1b8828e1de84e470a8f69d2ca
Arne Wiese <arne.wiese@gmail.com>
2019-08-23 18:07:58 +0200

    Add last_visit date

--- index.py
+++ index.py
@@ -1,23 +1,30 @@
 from flask import Flask, escape, request, make_response
+from datetime import datetime
 
 app = Flask(__name__)
 
-def update_visits(visit = 0):
+
+def update_visits(visit=0):
     try:
         return int(visit) + 1
     except ValueError:
-        return 0
+        return 1
     except:
-        return 0
+        return 1
 
 
 @app.route('/')
 def hello():
     #  log request cookies to console
     print(request.cookies)
-    visits = update_visits(request.cookies.get('page_visits'))
+    visits = update_visits(request.cookies.get('visits'))
+    last_visit = request.cookies.get('last_visit')
     name_from_request = request.args.get("name", "stranger")
-    resp = make_response(f'Hello {escape(name_from_request)}, you have visited this page {escape(visits)} times!')
-    resp.set_cookie("page_visits", str(visits))
-    resp.set_cookie("evil_tracking_cookie", name_from_request)
+
+    hello_text = f'Hello {escape(name_from_request)}, you have visited this page {escape(visits)} times!'
+    last_visit_text = f' Your last visit was on {escape(last_visit)}' if last_visit else ''
+
+    resp = make_response(hello_text + last_visit_text)
+    resp.set_cookie("visits", str(visits))
+    resp.set_cookie("last_visit", str(datetime.now().isoformat()))
     return resp
