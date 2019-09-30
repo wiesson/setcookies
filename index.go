bad4d1e9cf7e641dff76069276655f457d9addec
Arne <a.wiese@douglas.de>
2019-09-30 15:19:10 +0200

    Fix visits not being updated

--- index.go
+++ index.go
@@ -1,62 +1,62 @@
 package IndexHandler
 
 import (
-    "fmt"
-    "io"
-    "log"
-    "net/http"
-    "strconv"
-    "time"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
 )
 
 // addCookie will apply a new cookie to the response of a http
 // request, with the key/value this method is passed.
 func addCookie(w http.ResponseWriter, name string, value string) {
-    expire := time.Now().AddDate(0, 1, 0)
-    cookie := http.Cookie{
-        Name:    name,
-        Value:   value,
-        Expires: expire,
-        Secure: true,
-    }
-    http.SetCookie(w, &cookie)
+	expire := time.Now().AddDate(0, 1, 0)
+	cookie := http.Cookie{
+		Name:    name,
+		Value:   value,
+		Expires: expire,
+		Secure:  true,
+	}
+	http.SetCookie(w, &cookie)
 }
 
 func IndexHandler(w http.ResponseWriter, req *http.Request) {
-    visits := -1
-    lastVisit := ""
+	visits := 0
+	lastVisit := ""
 
-    for _, cookie := range req.Cookies() {
-        if cookie.Name == "visits" {
-            val, err := strconv.Atoi(cookie.Value)
-            if err != nil {
-                visits = 0
-                continue
-            }
-            if val > 0 {
-                visits = val
-            }
-        }
+	for _, cookie := range req.Cookies() {
+		if cookie.Name == "visits" {
+			val, err := strconv.Atoi(cookie.Value)
+			if err != nil {
+				visits = 0
+				continue
+			}
 
-        if cookie.Name == "last_visit" {
-            lastVisit = cookie.Value
-        }
-        log.Println("Cookie: ", cookie.Name, cookie.Value)
-    }
+			visits = val + 1
 
-    addCookie(w, "last_visit", time.Now().Format(time.RFC3339))
-    addCookie(w, "visits", strconv.Itoa(visits + 1))
+		}
 
-    responseStr := fmt.Sprintf("Hello, you have visited this page %d times.", visits)
+		if cookie.Name == "last_visit" {
+			lastVisit = cookie.Value
+		}
+		log.Println("Cookie: ", cookie.Name, cookie.Value)
+	}
 
-    if lastVisit != "" {
-        responseStr = responseStr + fmt.Sprintf(" Your last visit was on %s.", lastVisit)
-    }
+	addCookie(w, "last_visit", time.Now().Format(time.RFC3339))
+	addCookie(w, "visits", strconv.Itoa(visits))
 
-    io.WriteString(w, responseStr)
+	responseStr := fmt.Sprintf("Hello, you have visited this page %d times.", visits)
+
+	if lastVisit != "" {
+		responseStr = responseStr + fmt.Sprintf(" Your last visit was on %s.", lastVisit)
+	}
+
+	io.WriteString(w, responseStr)
 }
 
 func main() {
-    http.HandleFunc("/", IndexHandler)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", IndexHandler)
+	http.ListenAndServe(":8080", nil)
 }
