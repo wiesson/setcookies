7a786d630767985bba697d40fff62f78fe050573
Arne <a.wiese@douglas.de>
2019-09-30 13:37:20 +0200

    Remove name query parameter

--- index.py
+++ index.py
@@ -19,9 +19,8 @@ def hello():
     print(request.cookies)
     visits = update_visits(request.cookies.get('visits'))
     last_visit = request.cookies.get('last_visit')
-    name_from_request = request.args.get("name", "stranger")
 
-    hello_text = f'Hello {escape(name_from_request)}, you have visited this page {escape(visits)} times!'
+    hello_text = f'Hello stranger, you have visited this page {escape(visits)} times!'
     last_visit_text = f' Your last visit was on {escape(last_visit)}' if last_visit else ''
 
     resp = make_response(hello_text + last_visit_text)
